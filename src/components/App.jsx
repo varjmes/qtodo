d8d5d4f564a724f3471476838e5c0f20a0f1e5b5
Charlotte Spencer <charlottelaspencer@gmail.com>
2018-01-21 16:33:14 +0000

    refactor(app): use Fragment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
 import logo from "../logo.svg"
 import "../css/App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Fragment>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Fragment>
     )
   }
 }
