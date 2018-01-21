71f0ed0855721fe16d62db9602d73beca4f7e1a0
Charlotte Spencer <charlottelaspencer@gmail.com>
2018-01-21 16:27:26 +0000

    chore: create components and css folder

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { shallow } from "enzyme"
-import App from "../App"
+import App from "../components/App"
 
 describe("<App />", () => {
   test("should render without crashing", () => {
